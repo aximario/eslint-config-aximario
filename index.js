41887d955e5815f6f1e15731dfde13e0c659b2cc
aximario <aximario@qq.com>
2019-02-28 11:39:44 +0800

    Update: 增加了一些全局 env

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 module.exports = {
     extends: ['airbnb'],
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        mocha: true,
+        jest: true,
+        jasmine: true
+    },
     parser: 'babel-eslint',
     rules: {
         indent: ['error', 4]
